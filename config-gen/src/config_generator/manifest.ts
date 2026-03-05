18eaa2b546f10f3072737c75c5f7f39aad00f06f
MicahBird <31116011+MicahBird@users.noreply.github.com>
2026-03-05 09:14:59 -0700

    Update config-gen

--- config-gen/src/config_generator/manifest.ts
+++ config-gen/src/config_generator/manifest.ts
@@ -1,6 +1,16 @@
 export const manifest = {
   "apps": [
-    "templates/apps/calibre.nix.hbs"
+    "templates/apps/vaultwarden.nix.hbs",
+    "templates/apps/syncthing.nix.hbs",
+    "templates/apps/pi-hole.nix.hbs",
+    "templates/apps/navidrome.nix.hbs",
+    "templates/apps/n8n.nix.hbs",
+    "templates/apps/mealie.nix.hbs",
+    "templates/apps/jellyfin.nix.hbs",
+    "templates/apps/immich.nix.hbs",
+    "templates/apps/forgejo.nix.hbs",
+    "templates/apps/calibre.nix.hbs",
+    "templates/apps/actual-budget.nix.hbs"
   ],
   "services": [
     "templates/services/tailscale"
