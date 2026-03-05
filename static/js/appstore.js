8d504e3eb955df6cb098c6c244dceceb9d0bac03
MicahBird <31116011+MicahBird@users.noreply.github.com>
2026-03-05 09:43:06 -0700

    Add more templates

--- static/js/appstore.js
+++ static/js/appstore.js
@@ -11748,7 +11748,17 @@ var import_handlebars = __toESM(require_handlebars(), 1);
 // src/config_generator/manifest.ts
 var manifest = {
   "apps": [
-    "templates/apps/calibre.nix.hbs"
+    "templates/apps/vaultwarden.nix.hbs",
+    "templates/apps/syncthing.nix.hbs",
+    "templates/apps/pi-hole.nix.hbs",
+    "templates/apps/navidrome.nix.hbs",
+    "templates/apps/n8n.nix.hbs",
+    "templates/apps/mealie.nix.hbs",
+    "templates/apps/jellyfin.nix.hbs",
+    "templates/apps/immich.nix.hbs",
+    "templates/apps/forgejo.nix.hbs",
+    "templates/apps/calibre.nix.hbs",
+    "templates/apps/actual-budget.nix.hbs"
   ],
   "services": [
     "templates/services/tailscale"
